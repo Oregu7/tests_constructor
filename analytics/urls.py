3f2dc2f89b79a1d1757a107da7f6fa3357a24036
Oregu7 <jekkicool@yandex.ru>
2016-04-05 21:34:59 +0300

    add send .xls answers and questions

--- analytics/urls.py
+++ analytics/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import url, include
-from .views import index, get_page, save_analytics
+from .views import index, get_page, save_analytics, send_to_excel
 urlpatterns = [
     url(r'^$', index),
     url(r'^save/$', save_analytics),
-    url(r'^(?P<page>\S+)/$', get_page)
+    url(r'^(?P<page>\w+)/$', get_page),
+    url(r'^(?P<data>\w+)/(?P<test>\d+)/$', send_to_excel)
+
 ]
--- analytics/views.py
+++ analytics/views.py
@@ -1,8 +1,11 @@
-from django.shortcuts import render_to_response
-from django.http import HttpResponse, JsonResponse, QueryDict
+from django.shortcuts import render_to_response, get_object_or_404
+from django.http import HttpResponse, JsonResponse, QueryDict, Http404
 from django.views.decorators.csrf import csrf_exempt
-from constructor.models import Query, Answer
+from constructor.models import Query, Answer, Test
+from constructor.serializers import QuerySerializer, AnswerSerializer
+from django_excel import make_response_from_query_sets, make_response_from_records
 import json
+import pyexcel.ext.xls
 
 def index(request):
     return render_to_response('analytics.html')
@@ -21,3 +24,29 @@ def save_analytics(request):
             answer.save()
 
     return JsonResponse({'success': True})
+
+def send_to_excel(request, data, test):
+    test = get_object_or_404(Test, id=test)
+
+    if data == "answers":
+        answers = Answer.objects.filter(query__test=test)
+        serializer = AnswerSerializer(answers, many=True)
+        file_name = test.title + "_answers"
+        serializer = list(map(change_answers, serializer.data))
+        return make_response_from_records(serializer, 'xls', file_name=file_name)
+    elif data == "questions":
+        questions = Query.objects.filter(test=test)
+        file_name = test.title + "_questions"
+        column_names = ['id', 'text']
+        return make_response_from_query_sets(questions, column_names, 'xls', file_name=file_name)
+    else:
+        return Http404("Does Not Exist")
+
+def change_answers(answer):
+    response = {
+        'ID_Вопроса': answer['query'],
+        'Текст_Ответа': answer['text'],
+        'Аналитика': answer['analytics']
+    }
+
+    return response
